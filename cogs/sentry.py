48708c79c90f4b56adb42467e4f1c3c19e5ebf41
techfish <msuke1018@icloud.com>
2025-10-11 22:17:32 +0900

    feat: メトリクス用のカウンターを追加し、エラーとTTSのカウントを実装

--- cogs/sentry.py
+++ cogs/sentry.py
@@ -30,6 +30,7 @@ class SentryCog(commands.Cog):
             sys.__excepthook__(exc_type, exc_value, exc_traceback)
             return
         sentry_sdk.capture_exception(exc_value)
+        self.bot.error_counter += 1  # エラーカウンターをインクリメント
 
     def asyncio_exception_handler(self, loop, context):
         exception = context.get("exception")
@@ -37,6 +38,7 @@ class SentryCog(commands.Cog):
             sentry_sdk.capture_exception(exception)
         else:
             sentry_sdk.capture_message(str(context))
+        self.bot.error_counter += 1  # エラーカウンターをインクリメント
 
     def unraisable_exception_handler(self, unraisable):
         exc = unraisable.exc_value if hasattr(unraisable, "exc_value") else None
@@ -44,6 +46,7 @@ class SentryCog(commands.Cog):
             sentry_sdk.capture_exception(exc)
         else:
             sentry_sdk.capture_message(str(unraisable))
+        self.bot.error_counter += 1  # エラーカウンターをインクリメント
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -54,16 +57,19 @@ class SentryCog(commands.Cog):
     async def on_error(self, event_method, *args, **kwargs):
         # discord.pyのグローバルエラー
         sentry_sdk.capture_exception()
+        self.bot.error_counter += 1  # エラーカウンターをインクリメント
     
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         # コマンドエラー
         sentry_sdk.capture_exception(error)
+        self.bot.error_counter += 1  # エラーカウンターをインクリメント
 
     @commands.Cog.listener()
     async def on_app_command_error(self, interaction, error):
         # アプリケーションコマンドのエラー
         sentry_sdk.capture_exception(error)
+        self.bot.error_counter += 1  # エラーカウンターをインクリメント
 
 async def setup(bot):
     await bot.add_cog(SentryCog(bot))
